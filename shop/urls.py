1789b519a18bf2b0cafb76e9b25f0c7411170d87
Otajon Ravilov <otajonravilov2008@gmail.com>
2025-03-22 22:43:37 +0500

    first commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from shop.views import IndexView, ProductDetailView, CustomerListView
+from shop.views import IndexView, ProductDetailView, CustomerListView, RegisterView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('products_list/<int:pk>/', IndexView.as_view(), name='products_list_by_category'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('customers/', CustomerListView.as_view(), name='customers_list'),
+    path('register/',RegisterView.as_view(),name='register'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 from shop.models import Category, Product, Customer
+from django.contrib.auth import login
+from .forms import RegisterForm
 
 
 
@@ -30,3 +32,22 @@ class CustomerListView(ListView):
     model = Customer
     template_name = 'shop/customers.html'
     context_object_name = 'customers'
+
+
+
+
+
+class RegisterView(View):
+    template_name = "shop/register.html"
+
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Foydalanuvchini avtomatik login qilish
+            return redirect("shop:home")  # Asosiy sahifaga yo'naltirish
+        return render(request, self.template_name, {"form": form})
